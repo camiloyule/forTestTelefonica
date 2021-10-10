9c99d6414d9b02a7bb47d59bae0a83f5fb3b09ad
Camilo Yule <camilo.yule@gmail.com>
2021-10-09 20:18:13 -0500

    just testing

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 var inicialBoard;
 const xplayer='x';
 const yplayer='o';
-const initialPlayer= 'x';
-
+var initialPlayer= 'x';
+var currentPlayer='';
 const winnerPositions=[
     [0,1,2],
     [3,4,5],
@@ -20,7 +20,63 @@ const cells= document.querySelectorAll('.cell');
 start();
 
 function start(){
+    if(currentPlayer==''){
 
+        currentPlayer=xplayer;
+    }
+   
     initialBoard= Array.from(Array(9).keys());
-    console.log(initialBoard)
+    
+    console.log(cells)
+    for (var i = 0; i < cells.length; i++) {
+		
+		
+		cells[i].addEventListener('click', pickOne, false);
+	}
+}
+
+
+function pickOne(block){
+    console.log(block);
+    // debugger;
+    var blockid= block.target.id;
+    var blockTouched= document.getElementById(blockid);
+    var touched;
+    if(currentPlayer=='x'){
+
+        touched= blockTouched.querySelector('.xtrick');
+    }else{
+        touched= blockTouched.querySelector('.otrick');
+    }
+    touched.classList.add('activeTrick');
+    console.log(blockTouched);
+    changePlayer();
+   
+}
+
+function changePlayer(){
+
+if(currentPlayer=='x')
+{
+    currentPlayer='o';
+}else{
+    currentPlayer='x';
+}
+
+}
+
+function reStart(){
+    if(initialPlayer=='x')
+{
+    initialPlayer='o';
+}else{
+    initialPlayer='x';
+} 
+// debugger;
+    var doneTricks= document.querySelectorAll('.activeTrick');
+    for (var i = 0; i < doneTricks.length; i++) {
+		
+		
+		doneTricks[i].classList.remove('activeTrick')
+	}
 }
